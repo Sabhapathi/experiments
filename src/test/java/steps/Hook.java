4e023f0787da53a3ae94b98987cd0f06c923ba5f
Sabhapathi Akkipalli <Sabhapathi.Akkipalli@clicksoftware.com>
2017-10-31 14:40:17 -0500

    added appium settings

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -4,11 +4,14 @@ import base.BaseUtil;
 import cucumber.api.Scenario;
 import cucumber.api.java.Before;
 import cucumber.api.java.After;
+import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
@@ -16,13 +19,17 @@ import java.util.concurrent.TimeUnit;
 public class Hook extends BaseUtil {
 
     private BaseUtil base;
+    protected AndroidDriver mobiledriver;
+    public static final String appName = "TimeTrack_Sprint.apk";
+    public static final String APP_DIR = System.getProperty("user.dir") + "\\src\\apps";
+    public static java.lang.Process Process;
 
     public Hook(BaseUtil base) {
         this.base = base;
     }
 
     @Before
-    public void InitializeTest() throws MalformedURLException {
+    public void InitializeTest() throws IOException, InterruptedException {
 
 
         //************        CHROME Driver         *******//
@@ -58,6 +65,25 @@ public class Hook extends BaseUtil {
         base.Driver.manage().window().maximize();
         base.Driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
+
+        // Appium Server
+        InitializeAppiumServer();
+
+        //Android Mobile Driver
+
+        File appDir = new File(APP_DIR);
+        File app = new File(appDir, appName);
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("device", "Device");
+        caps.setCapability("deviceName", "Pixel");
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("app", app.getAbsolutePath());
+        caps.setCapability("newCommandTimeout", 60);
+
+        mobiledriver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+
+
+
     }
 
     @After
@@ -70,6 +96,19 @@ public class Hook extends BaseUtil {
 
         base.Driver.close();
         base.Driver.quit();
+        mobiledriver.closeApp();
+        mobiledriver.close();
+        mobiledriver.quit();
+    }
+
+    public  void InitializeAppiumServer() throws IOException,InterruptedException{
+        if(Process == null)
+        {
+            Process= Runtime.getRuntime().exec("cmd /c start "+System.getProperty("user.dir") + "\\src\\apps\\startappium.bat");
+            System.out.println("Appium starts..");
+            Thread.sleep(10000);
+        }
+
     }
 
 }
